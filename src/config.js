530d9494abfdd6038289d8d66b552491524a91e0
ahadhossainaiman <ahadhossainaiman017@gmail.com>
2024-05-05 12:08:25 +0600

    api domain change

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const baseURL = axios.create({
-     baseURL:"http://170.64.212.91:3232/v1",
+     baseURL:"https://api.spotlyt.co.za/v1",
      timeout:10000,
      headers:{"X-Custom-Header":"foobar", authorization:`Bearer ${localStorage.getItem("token")}`},
 })
--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-      baseUrl: 'http://170.64.212.91:3232/v1/',
+      baseUrl: 'https://api.spotlyt.co.za/v1/',
       headers:{
           "Content-Type": "application/json",
           authorization:`Bearer ${localStorage.getItem("token")}`
